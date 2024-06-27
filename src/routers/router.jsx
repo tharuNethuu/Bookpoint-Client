32bc36da170282b1fd5024045dc12960d53ad919
tharuNethuu <150820780+tharuNethuu@users.noreply.github.com>
2024-06-28 00:39:11 +0530

    Update router.jsx

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,5 +1,8 @@
- import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from "../App";
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import App from "..//App";
 import Home from "../home/Home";
 import Shop from "../shop/Shop";
 import About from "../components/About";
@@ -24,101 +27,103 @@ import NotificationsPage from "../components/NotificationsPage";
 import LoginPage from "../DeliverySec/LoginPage";
 import ProvinceOrders from "../DeliverySec/ProvinceOrders";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: "/shop",
-        element: <PrivateRoute><ShopNow /></PrivateRoute>
-      },
-      {
-        path: "/about",
-        element: <About />
-      },
-      {
-        path: "/blog",
-        element: <Blog />
-      },
-      {
-        path: "/AwardBooks",
-        element: <AwardBooks />
-      },
-      {
-        path: "logout",
-        element: <Logout />
-      },
-      {
-        path: "wishlist-details",
-        element: <WishlistDetails />
-      },
-      {
-        path: "/book/:id",
-        element: <SingleBook />,
-        loader: ({ params }) => fetch(https://bookpoint-api.vercel.app/books/${params.id})
-      }
-    ]
-  },
-  {
-    path: "/admin/dashboard",
-    element: <Dashboardlayout />,
-    children: [
-      {
-        path: "/admin/dashboard",
-        element: <PrivateRoute><Dashboard /></PrivateRoute>
-      },
-      {
-        path: "/admin/dashboard/upload",
-        element: <UploadBooks />
-      },
-      {
-        path: "/admin/dashboard/manage",
-        element: <ManageBooks />
-      },
-      {
-        path: "/admin/dashboard/orders",
-        element: <Orderpage />
-      },
-      {
-        path: "/admin/dashboard/edit-books/:id",
-        element: <EditBooks />,
-        loader: ({ params }) => fetch(https://bookpoint-api.vercel.app/books/${params.id})
-      },
-      {
-        path: "/admin/dashboard/notifications/:email",
-        element: <Notifications />
-      }
-    ]
-  },
-  {
-    path: "/sign-up",
-    element: <Signup />
-  },
-  {
-    path: "login",
-    element: <Login />
-  },
-  {
-    path: "/adminIn",
-    element: <AdminIn />
-  },
-  {
-    path: "/notifications/:email",
-    element: <PrivateRoute><NotificationsPage /></PrivateRoute>
-  },
-  {
-    path: "/deliverylogin",
-    element: <LoginPage />
-  },
-  {
-    path: "/ordersdelivery",
-    element: <ProvinceOrders />
-  }
-]);
 
-export default router;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children: [
+        {
+            path: '/',
+            element: <Home/>
+        },
+        {
+            path: "/shop",
+            element: <PrivateRoute><ShopNow/></PrivateRoute>        },
+
+        {
+            path: "/about",
+            element: <About/>
+        },
+        {
+            path: "/blog" ,
+            element: <Blog/>
+        },
+        {
+          path: "/AwardBooks" ,
+          element: <AwardBooks/>
+      },{
+        path:"logout",
+        element: <Logout/>
+      },{
+        path:"wishlist-details",
+        element: <WishlistDetails/>
+      },
+
+        {
+          path: "/book/:id",
+          element: <SingleBook/>,
+          loader:({params}) => fetch (`https://bookpoint-api.vercel.app/books/${params.id}`)
+        },
+
+      ]
+    },
+    {
+      path: "/admin/dashboard",
+      element: <Dashboardlayout/>,
+      children:[
+        {
+          path: "/admin/dashboard",
+          element: <PrivateRoute><Dashboard/></PrivateRoute>
+        
+        },
+        {
+          path: "/admin/dashboard/upload",
+          element: <UploadBooks/>
+        
+        },
+        
+        {
+          path: "/admin/dashboard/manage",
+          element: <ManageBooks/>
+        
+        },{
+          path:"/admin/dashboard/orders",
+          element: <Orderpage/>
+        }, 
+        {
+          path: "/admin/dashboard/edit-books/:id",
+          element: <EditBooks/>,
+          loader:({params}) => fetch (`https://bookpoint-api.vercel.app/books/${params.id}`)
+        
+        },{
+          path: "/admin/dashboard/notifications/:email",
+          element: <Notifications/>
+        }
+        
+      ]
+    },
+    {
+      path:"/sign-up",
+      element: <Signup/>
+    },
+    {
+      path:"login",
+      element: <Login/>
+    },{
+      path: "/adminIn",
+      element: <AdminIn/>
+    },{
+      path: "/notifications/:email",
+      element: <PrivateRoute><NotificationsPage/></PrivateRoute> 
+
+    },{
+      path: "/deliverylogin",
+      element: <LoginPage/>
+    },{
+      path:"/ordersdelivery" ,
+      element: <ProvinceOrders/>
+    }
+  ]);
+
+  export default router;
